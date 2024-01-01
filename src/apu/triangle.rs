650a36d4fb0b24739593967a1f87d078828fe9ab
lukasz050792 <155379384+lukasz050792@users.noreply.github.com>
2024-01-01 18:08:23 +0100

    Update triangle.rs

--- src/apu/triangle.rs
+++ src/apu/triangle.rs
@@ -1,207 +1,140 @@
-use crate::save_load::*;
+use crate::{
+    apu::{length_counter::LengthCounter, linear_counter::LinearCounter},
+    common::{Clock, Kind, Reset},
+};
+use serde::{Deserialize, Serialize};
 
-use super::length_counter::LengthCounterState;
-use super::audio_channel::AudioChannelState;
-use super::audio_channel::PlaybackRate;
-use super::audio_channel::Volume;
-use super::audio_channel::Timbre;
-use super::ring_buffer::RingBuffer;
-use super::filters;
-use super::filters::DspFilter;
-
-pub struct TriangleChannelState {
-    pub name: String,
-    pub chip: String,
-    pub debug_disable: bool,
-    pub output_buffer: RingBuffer,
-    pub edge_buffer: RingBuffer,
-    pub last_edge: bool,
-    pub debug_filter: filters::HighPassIIR,
-    pub length_counter: LengthCounterState,
-
-    pub control_flag: bool,
-    pub linear_reload_flag: bool,
-    pub linear_counter_initial: u8,
-    pub linear_counter_current: u8,
-
-    pub sequence_counter: u8,
-    pub period_initial: u16,
-    pub period_current: u16,
-    pub length: u8,
-
-    pub cpu_clock_rate: u64,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[must_use]
+pub struct Triangle {
+    enabled: bool,
+    force_silent: bool,
+    ultrasonic: bool,
+    step: u8,
+    freq_timer: u16,
+    freq_counter: u16,
+    length: LengthCounter,
+    linear: LinearCounter,
 }
 
-impl TriangleChannelState {
-    pub fn new(channel_name: &str, chip_name: &str, cpu_clock_rate: u64) -> TriangleChannelState {
-        return TriangleChannelState {
-            name: String::from(channel_name),
-            chip: String::from(chip_name),
-            debug_disable: false,
-            output_buffer: RingBuffer::new(32768),
-            last_edge: false,
-            debug_filter: filters::HighPassIIR::new(44100.0, 300.0),
-            edge_buffer: RingBuffer::new(32768),
-            length_counter: LengthCounterState::new(),
-            control_flag: false,
-            linear_reload_flag: false,
-            linear_counter_initial: 0,
-            linear_counter_current: 0,
+impl Default for Triangle {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
-            sequence_counter: 0,
-            period_initial: 0,
-            period_current: 0,
-            length: 0,
-
-            cpu_clock_rate: cpu_clock_rate,
+impl Triangle {
+    pub const fn new() -> Self {
+        Self {
+            enabled: false,
+            force_silent: false,
+            ultrasonic: false,
+            step: 0u8,
+            freq_timer: 0u16,
+            freq_counter: 0u16,
+            length: LengthCounter::new(),
+            linear: LinearCounter::new(),
         }
     }
 
-    pub fn update_linear_counter(&mut self) {
-        if self.linear_reload_flag {
-            self.linear_counter_current = self.linear_counter_initial;
+    #[inline]
+    #[must_use]
+    pub const fn silent(&self) -> bool {
+        self.force_silent
+    }
+
+    #[inline]
+    pub fn toggle_silent(&mut self) {
+        self.force_silent = !self.force_silent;
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn length_counter(&self) -> u8 {
+        self.length.counter()
+    }
+
+    pub fn clock_quarter_frame(&mut self) {
+        if self.linear.reload {
+            self.linear.counter = self.linear.load;
+        } else if self.linear.counter > 0 {
+            self.linear.counter -= 1;
+        }
+        if !self.linear.control {
+            self.linear.reload = false;
+        }
+    }
+
+    #[inline]
+    pub fn clock_half_frame(&mut self) {
+        self.length.clock();
+    }
+
+    #[must_use]
+    pub fn output(&self) -> f32 {
+        if self.force_silent {
+            0.0
+        } else if self.ultrasonic {
+            7.5
+        } else if self.step & 0x10 == 0x10 {
+            f32::from(self.step ^ 0x1F)
         } else {
-            if self.linear_counter_current > 0 {
-                self.linear_counter_current -= 1;
-            }
-        }
-        if !(self.control_flag) {
-            self.linear_reload_flag = false;
+            f32::from(self.step)
         }
     }
 
-    pub fn clock(&mut self) {
-        if self.linear_counter_current != 0 && self.length_counter.length > 0 {
-            if self.period_current == 0 {
-                // Reset the period timer, and clock the waveform generator
-                self.period_current = self.period_initial;
+    pub fn write_linear_counter(&mut self, val: u8) {
+        self.linear.control = (val >> 7) & 1 == 1; // D7
+        self.length.enabled = (val >> 7) & 1 == 0; // !D7
+        self.linear.load_value(val);
+    }
 
-                // The sequence counter starts at zero, but counts downwards, resulting in an odd
-                // lookup sequence of 0, 7, 6, 5, 4, 3, 2, 1
-                if self.sequence_counter >= 31 {
-                    self.sequence_counter = 0;
-                    self.last_edge = true;
-                } else {
-                    self.sequence_counter += 1;
-                }
+    pub fn write_timer_lo(&mut self, val: u8) {
+        self.freq_timer = (self.freq_timer & 0xFF00) | u16::from(val); // D7..D0
+    }
+
+    pub fn write_timer_hi(&mut self, val: u8) {
+        self.freq_timer = (self.freq_timer & 0x00FF) | u16::from(val & 0x07) << 8; // D2..D0
+        self.freq_counter = self.freq_timer;
+        self.linear.reload = true;
+        if self.enabled {
+            self.length.load_value(val);
+        }
+    }
+
+    pub fn set_enabled(&mut self, enabled: bool) {
+        self.enabled = enabled;
+        if !enabled {
+            self.length.counter = 0;
+        }
+    }
+}
+
+impl Clock for Triangle {
+    fn clock(&mut self) -> usize {
+        self.ultrasonic = false;
+        if self.length.counter > 0 && self.freq_timer < 2 && self.freq_counter == 0 {
+            self.ultrasonic = true;
+        }
+
+        let should_clock =
+            !(self.length.counter == 0 || self.linear.counter == 0 || self.ultrasonic);
+        if should_clock {
+            if self.freq_counter > 0 {
+                self.freq_counter -= 1;
             } else {
-                self.period_current -= 1;
+                self.freq_counter = self.freq_timer;
+                self.step = (self.step + 1) & 0x1F;
             }
+            1
         } else {
-            // When the triangle is disabled, treat every sample as the last edge. This prevents
-            // holding onto the trailing end of the waveform in the debug display for too long
-            self.last_edge = true;
+            0
         }
-    }
-
-    pub fn output(&self) -> i16 {
-        if self.period_initial <= 2 {
-            // This frequency is so high that the hardware mixer can't keep up, and effectively
-            // receives 7.5. We'll just return 7 here (close enough). Some games use this
-            // to silence the channel, and returning 7 emulates the resulting clicks and pops.
-            return 7;
-        } else {
-            let triangle_sequence = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
-                                     15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0];
-            return triangle_sequence[self.sequence_counter as usize];
-        }
-    }
-
-    pub fn save_state(&self, buff: &mut Vec<u8>) {
-        self.length_counter.save_state(buff);
-        save_bool(buff, self.control_flag);
-        save_bool(buff, self.linear_reload_flag);
-        save_u8(buff, self.linear_counter_initial);
-        save_u8(buff, self.linear_counter_current);
-        save_u8(buff, self.sequence_counter);
-        save_u16(buff, self.period_initial);
-        save_u16(buff, self.period_current);
-        save_u8(buff, self.length);
-        save_u64(buff, self.cpu_clock_rate);
-    }
-
-    pub fn load_state(&mut self, buff: &mut Vec<u8>) {
-        load_u64(buff, &mut self.cpu_clock_rate);
-        load_u8(buff, &mut self.length);
-        load_u16(buff, &mut self.period_current);
-        load_u16(buff, &mut self.period_initial);
-        load_u8(buff, &mut self.sequence_counter);
-        load_u8(buff, &mut self.linear_counter_current);
-        load_u8(buff, &mut self.linear_counter_initial);
-        load_bool(buff, &mut self.linear_reload_flag);
-        load_bool(buff, &mut self.control_flag);
-        self.length_counter.load_state(buff);
     }
 }
 
-impl AudioChannelState for TriangleChannelState {
-    fn name(&self) -> String {
-        return self.name.clone();
-    }
-
-    fn chip(&self) -> String {
-        return self.chip.clone();
-    }
-
-    fn sample_buffer(&self) -> &RingBuffer {
-        return &self.output_buffer;
-    }
-
-    fn edge_buffer(&self) -> &RingBuffer {
-        return &self.edge_buffer;
-    }
-
-    fn record_current_output(&mut self) {
-        self.debug_filter.consume(self.output() as f32);
-        self.output_buffer.push((self.debug_filter.output() * -4.0) as i16);
-        self.edge_buffer.push(self.last_edge as i16);
-        self.last_edge = false;
-    }
-
-    fn min_sample(&self) -> i16 {
-        return -60;
-    }
-
-    fn max_sample(&self) -> i16 {
-        return 60;
-    }
-
-    fn muted(&self) -> bool {
-        return self.debug_disable;
-    }
-
-    fn mute(&mut self) {
-        self.debug_disable = true;
-    }
-
-    fn unmute(&mut self) {
-        self.debug_disable = false;
-    }
-
-    fn playing(&self) -> bool {
-        return 
-            self.length_counter.length > 0 && 
-            self.linear_counter_current != 0 &&
-            self.period_initial > 2;
-    }
-
-    fn rate(&self) -> PlaybackRate {
-        let frequency = self.cpu_clock_rate as f32 / (32.0 * (self.period_initial as f32 + 1.0));
-        return PlaybackRate::FundamentalFrequency {frequency: frequency};
-    }
-
-    fn volume(&self) -> Option<Volume> {
-        return None;
-    }
-
-    fn timbre(&self) -> Option<Timbre> {
-        return None;
-    }
-
-    fn amplitude(&self) -> f32 {
-        if self.playing() {
-            return 0.55;
-        }
-        return 0.0;
+impl Reset for Triangle {
+    fn reset(&mut self, _kind: Kind) {
+        *self = Self::new();
     }
 }
