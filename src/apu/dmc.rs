e83e8c75960d9fae18978fdf451875bb6d18db86
lukasz050792 <155379384+lukasz050792@users.noreply.github.com>
2024-01-01 18:12:23 +0100

    Update dmc.rs

--- src/apu/dmc.rs
+++ src/apu/dmc.rs
@@ -1,256 +1,289 @@
-use crate::mmc::mapper::Mapper;
-use crate::save_load::*;
-use super::audio_channel::AudioChannelState;
-use super::ring_buffer::RingBuffer;
-use super::filters;
-use super::filters::DspFilter;
+use crate::common::{Clock, Kind, NesRegion, Regional, Reset};
+use serde::{Deserialize, Serialize};
 
-pub struct DmcState {
-    pub name: String,
-    pub chip: String,
-    pub debug_disable: bool,
-    pub output_buffer: RingBuffer,
-    pub edge_buffer: RingBuffer,
-    pub last_edge: bool,
-    pub debug_filter: filters::HighPassIIR,
-
-    pub looping: bool,
-    pub period_initial: u16,
-    pub period_current: u16,
-    pub output_level: u8,
-    pub starting_address: u16,
-    pub sample_length: u16,
-
-    pub current_address: u16,
-    pub sample_buffer: u8,
-    pub shift_register: u8,
-    pub sample_buffer_empty: bool,
-    pub bits_remaining: u8,
-    pub bytes_remaining: u16,
-    pub silence_flag: bool,
-
-    pub interrupt_enabled: bool,
-    pub interrupt_flag: bool,
-    pub rdy_line: bool,
-    pub rdy_delay: u8,
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+#[must_use]
+pub struct Dmc {
+    region: NesRegion,
+    force_silent: bool,
+    irq_enabled: bool,
+    irq_pending: bool,
+    loops: bool,
+    freq_timer: u16,
+    freq_counter: u16,
+    addr: u16,
+    addr_load: u16,
+    length: u16,
+    length_load: u16,
+    sample_buffer: u8,
+    sample_buffer_empty: bool,
+    dma_pending: bool,
+    init: u8,
+    output: u8,
+    output_bits: u8,
+    output_shift: u8,
+    output_silent: bool,
 }
 
-impl DmcState {
-    pub fn new(channel_name: &str, chip_name: &str) -> DmcState {
-        return DmcState {
-            name: String::from(channel_name),
-            chip: String::from(chip_name),
-            debug_disable: false,
-            output_buffer: RingBuffer::new(32768),
-            edge_buffer: RingBuffer::new(32768),
-            last_edge: false,
-            debug_filter: filters::HighPassIIR::new(44100.0, 300.0),
+impl Default for Dmc {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
-            looping: false,
-            period_initial: 428,
-            period_current: 0,
-            output_level: 0,
-            starting_address: 0,
-            sample_length: 0,
+impl Dmc {
+    const FREQ_TABLE_NTSC: [u16; 16] = [
+        0x1AC, 0x17C, 0x154, 0x140, 0x11E, 0x0FE, 0x0E2, 0x0D6, 0x0BE, 0x0A0, 0x08E, 0x080, 0x06A,
+        0x054, 0x048, 0x036,
+    ];
+    const FREQ_TABLE_PAL: [u16; 16] = [
+        0x18E, 0x162, 0x13C, 0x12A, 0x114, 0x0EC, 0x0D2, 0x0C6, 0x0B0, 0x094, 0x084, 0x076, 0x062,
+        0x04E, 0x042, 0x032,
+    ];
 
-            current_address: 0,
-            sample_buffer: 0,
-            shift_register: 0,
+    pub fn new() -> Self {
+        let region = NesRegion::default();
+        let freq_timer = Self::freq_timer(region, 0);
+        Self {
+            region,
+            force_silent: false,
+            irq_enabled: false,
+            irq_pending: false,
+            loops: false,
+            freq_timer,
+            freq_counter: freq_timer,
+            addr: 0xC000,
+            addr_load: 0x0000,
+            length: 0x0000,
+            length_load: 0x0001,
+            sample_buffer: 0x00,
             sample_buffer_empty: true,
-            bits_remaining: 8,
-            bytes_remaining: 0,
-            silence_flag: false,
-            interrupt_enabled: true,
-            interrupt_flag: false,
-            rdy_line: false,
-            rdy_delay: 0,
+            dma_pending: false,
+            init: 0,
+            output: 0x00,
+            output_bits: 0x00,
+            output_shift: 0x00,
+            output_silent: true,
         }
     }
 
-    pub fn debug_status(&self) -> String {
-        return format!("Rate: {:3} - Divisor: {:3} - Start: {:04X} - Current: {:04X} - Length: {:4} - R.Bytes: {:4} - R.Bits: {:1}", 
-            self.period_initial, self.period_current, self.starting_address, self.current_address, self.sample_length,
-            self.bytes_remaining, self.bits_remaining);
+    #[inline]
+    #[must_use]
+    pub const fn silent(&self) -> bool {
+        self.force_silent
     }
 
-    pub fn read_next_sample(&mut self, mapper: &mut dyn Mapper) {
-        match mapper.read_cpu(0x8000 | (self.current_address & 0x7FFF)) {
-            Some(byte) => self.sample_buffer = byte,
-            None => self.sample_buffer = 0,
-        }
-        self.current_address = self.current_address.wrapping_add(1);
-        self.bytes_remaining -= 1;
-        if self.bytes_remaining == 0 {
-            if self.looping {
-                self.current_address = self.starting_address;
-                self.bytes_remaining = self.sample_length;
-                self.last_edge = true;
-            } else {
-                if self.interrupt_enabled {
-                    self.interrupt_flag = true;
+    #[inline]
+    pub fn toggle_silent(&mut self) {
+        self.force_silent = !self.force_silent;
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn length(&self) -> u16 {
+        self.length
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn irq_enabled(&self) -> bool {
+        self.irq_enabled
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn irq_pending(&self) -> bool {
+        self.irq_pending
+    }
+
+    #[inline]
+    pub fn acknowledge_irq(&mut self) {
+        self.irq_pending = false;
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn dma(&mut self) -> bool {
+        let pending = self.dma_pending;
+        self.dma_pending = false;
+        pending
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn dma_addr(&self) -> u16 {
+        self.addr
+    }
+
+    #[inline]
+    pub fn load_buffer(&mut self, val: u8) {
+        self.dma_pending = false;
+        if self.length > 0 {
+            self.sample_buffer = val;
+            self.sample_buffer_empty = false;
+            self.addr = self.addr.wrapping_add(1);
+            if self.addr == 0 {
+                self.addr = 0x8000;
+            }
+            self.length -= 1;
+            if self.length == 0 {
+                if self.loops {
+                    self.length = self.length_load;
+                    self.addr = self.addr_load;
+                } else if self.irq_enabled {
+                    self.irq_pending = true;
                 }
             }
         }
-        self.sample_buffer_empty = false;
-        self.rdy_line = false;
-        self.rdy_delay = 0;
     }
 
-    pub fn begin_output_cycle(&mut self) {
-        self.bits_remaining = 8;
-        if self.sample_buffer_empty {
-            self.silence_flag = true;
-        } else {
-            self.silence_flag = false;
-            self.shift_register = self.sample_buffer;
-            self.sample_buffer_empty = true;
+    #[inline]
+    const fn freq_timer(region: NesRegion, val: u8) -> u16 {
+        match region {
+            NesRegion::Ntsc => Self::FREQ_TABLE_NTSC[(val & 0x0F) as usize] - 2,
+            NesRegion::Pal | NesRegion::Dendy => Self::FREQ_TABLE_PAL[(val & 0x0F) as usize] - 2,
         }
     }
 
-    pub fn update_output_unit(&mut self) {
-        if !(self.silence_flag) {
-            let mut target_output = self.output_level;
-            if (self.shift_register & 0b1) == 0 {
-                if self.output_level >= 2 {
-                    target_output -= 2;
-                }
-            } else  {
-                if self.output_level <= 125 {
-                    target_output += 2;
-                }
+    #[inline]
+    #[must_use]
+    pub fn output(&self) -> f32 {
+        if self.force_silent {
+            0.0
+        } else {
+            f32::from(self.output)
+        }
+    }
+
+    // $4010 DMC timer
+    pub fn write_timer(&mut self, val: u8) {
+        self.irq_enabled = val & 0x80 == 0x80;
+        self.loops = val & 0x40 == 0x40;
+        self.freq_timer = Self::freq_timer(self.region, val);
+        if !self.irq_enabled {
+            self.irq_pending = false;
+        }
+    }
+
+    // $4011 DMC output
+    #[inline]
+    pub fn write_output(&mut self, val: u8) {
+        self.output = val;
+    }
+
+    // $4012 DMC addr load
+    #[inline]
+    pub fn write_addr_load(&mut self, val: u8) {
+        self.addr_load = 0xC000 | (u16::from(val) << 6);
+    }
+
+    // $4013 DMC length
+    #[inline]
+    pub fn write_length(&mut self, val: u8) {
+        self.length_load = (u16::from(val) << 4) + 1;
+    }
+
+    // $4015 WRITE
+    pub fn set_enabled(&mut self, enabled: bool, cycle: usize) {
+        self.irq_pending = false;
+        if !enabled {
+            self.length = 0;
+        } else if self.length == 0 {
+            self.addr = self.addr_load;
+            self.length = self.length_load;
+            // Delay a number of cycles based on even/odd cycle
+            self.init = if cycle & 0x01 == 0x00 { 2 } else { 3 };
+        }
+    }
+
+    #[inline]
+    pub fn check_pending_dma(&mut self) {
+        if self.init > 0 {
+            self.init -= 1;
+            if self.init == 0 && self.sample_buffer_empty && self.length > 0 {
+                self.dma_pending = true;
             }
-            self.output_level = target_output;
         }
-        self.shift_register = self.shift_register >> 1;
-        self.bits_remaining -= 1;
-        if self.bits_remaining == 0 {
-            self.begin_output_cycle();
-        }
-    }
-
-    pub fn clock(&mut self, mapper: &mut dyn Mapper) {
-        if self.period_current == 0 {
-            self.period_current = self.period_initial - 1;
-            self.update_output_unit();
-        } else {
-            self.period_current -= 1;
-        }
-        if self.sample_buffer_empty && self.bytes_remaining > 0 {
-            self.rdy_line = true;
-            self.rdy_delay += 1;
-            if self.rdy_delay > 2 {
-                self.read_next_sample(mapper);
-            }
-        } else {
-            self.rdy_line = false;
-            self.rdy_delay = 0;
-        }
-    }
-
-    pub fn output(&self) -> i16 {
-        return self.output_level as i16;
-    }
-
-    pub fn save_state(&self, buff: &mut Vec<u8>) {
-        save_bool(buff, self.looping);
-        save_u16(buff, self.period_initial);
-        save_u16(buff, self.period_current);
-        save_u8(buff, self.output_level);
-        save_u16(buff, self.starting_address);
-        save_u16(buff, self.sample_length);
-        save_u16(buff, self.current_address);
-        save_u8(buff, self.sample_buffer);
-        save_u8(buff, self.shift_register);
-        save_bool(buff, self.sample_buffer_empty);
-        save_u8(buff, self.bits_remaining);
-        save_u16(buff, self.bytes_remaining);
-        save_bool(buff, self.silence_flag);
-        save_bool(buff, self.interrupt_enabled);
-        save_bool(buff, self.interrupt_flag);
-        save_bool(buff, self.rdy_line);
-        save_u8(buff, self.rdy_delay);
-    }
-
-    pub fn load_state(&mut self, buff: &mut Vec<u8>) {
-        load_u8(buff, &mut self.rdy_delay);
-        load_bool(buff, &mut self.rdy_line);
-        load_bool(buff, &mut self.interrupt_flag);
-        load_bool(buff, &mut self.interrupt_enabled);
-        load_bool(buff, &mut self.silence_flag);
-        load_u16(buff, &mut self.bytes_remaining);
-        load_u8(buff, &mut self.bits_remaining);
-        load_bool(buff, &mut self.sample_buffer_empty);
-        load_u8(buff, &mut self.shift_register);
-        load_u8(buff, &mut self.sample_buffer);
-        load_u16(buff, &mut self.current_address);
-        load_u16(buff, &mut self.sample_length);
-        load_u16(buff, &mut self.starting_address);
-        load_u8(buff, &mut self.output_level);
-        load_u16(buff, &mut self.period_current);
-        load_u16(buff, &mut self.period_initial);
-        load_bool(buff, &mut self.looping);
     }
 }
 
-impl AudioChannelState for DmcState {
-    fn name(&self) -> String {
-        return self.name.clone();
-    }
+impl Clock for Dmc {
+    fn clock(&mut self) -> usize {
+        // Because APU is only clocked every other CPU cycle
+        if self.freq_counter >= 2 {
+            self.freq_counter -= 2;
+        } else {
+            self.freq_counter = self.freq_timer;
 
-    fn chip(&self) -> String {
-        return self.chip.clone();
-    }
+            if !self.output_silent {
+                if self.output_shift & 0x01 == 0x01 {
+                    if self.output <= 125 {
+                        self.output += 2;
+                    }
+                } else if self.output >= 2 {
+                    self.output -= 2;
+                }
+                self.output_shift >>= 1;
+            }
 
-    fn edge_buffer(&self) -> &RingBuffer {
-        return &self.edge_buffer;
-    }
-
-    fn sample_buffer(&self) -> &RingBuffer {
-        return &self.output_buffer;
-    }
-
-    fn record_current_output(&mut self) {
-        self.debug_filter.consume(self.output() as f32);
-        self.output_buffer.push((self.debug_filter.output() * -4.0) as i16);
-        self.edge_buffer.push(self.last_edge as i16);
-        self.last_edge = false;
-    }
-
-    fn min_sample(&self) -> i16 {
-        return -512;
-    }
-
-    fn max_sample(&self) -> i16 {
-        return 512;
-    }
-
-    fn muted(&self) -> bool {
-        return self.debug_disable;
-    }
-
-    fn mute(&mut self) {
-        self.debug_disable = true;
-    }
-
-    fn unmute(&mut self) {
-        self.debug_disable = false;
-    }
-
-    fn playing(&self) -> bool {
-        return self.amplitude() > 0.0;
-    }
-
-    fn amplitude(&self) -> f32 {
-        let buffer = self.output_buffer.buffer();
-        let mut index = (self.output_buffer.index() - 256) % buffer.len();
-        let mut max = buffer[index];
-        let mut min = buffer[index];
-        for _i in 0 .. 256 {
-            if buffer[index] > max {max = buffer[index];}
-            if buffer[index] < min {min = buffer[index];}
-            index += 1;
-            index = index % buffer.len();
+            self.output_bits = self.output_bits.saturating_sub(1);
+            if self.output_bits == 0 {
+                self.output_bits = 8;
+                if self.sample_buffer_empty {
+                    self.output_silent = true;
+                } else {
+                    self.output_silent = false;
+                    self.output_shift = self.sample_buffer;
+                    self.sample_buffer_empty = true;
+                    if self.length > 0 {
+                        self.dma_pending = true;
+                    }
+                }
+            }
         }
-        return (max - min) as f32 / 256.0;
+        1
+    }
+}
+
+impl Regional for Dmc {
+    #[inline]
+    fn region(&self) -> NesRegion {
+        self.region
+    }
+
+    #[inline]
+    fn set_region(&mut self, region: NesRegion) {
+        self.region = region;
+        self.freq_timer = Self::freq_timer(region, 0);
+    }
+}
+
+impl Reset for Dmc {
+    fn reset(&mut self, kind: Kind) {
+        self.irq_enabled = false;
+        self.irq_pending = false;
+        self.loops = false;
+        self.freq_timer = Self::freq_timer(self.region, 0);
+        self.freq_counter = self.freq_timer;
+        match kind {
+            Kind::Soft => {
+                self.addr = 0x0000;
+                self.length_load = 0x0000;
+            }
+            Kind::Hard => {
+                self.addr = 0xC000;
+                self.length_load = 0x0001;
+            }
+        }
+        self.addr_load = 0x0000;
+        self.length = 0x0000;
+        self.sample_buffer = 0x00;
+        self.sample_buffer_empty = true;
+        self.dma_pending = false;
+        self.init = 0;
+        self.output = 0x00;
+        self.output_bits = 0x00;
+        self.output_shift = 0x00;
+        self.output_silent = true;
     }
 }
