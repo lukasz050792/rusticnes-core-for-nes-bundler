e486333d312ab57ee736e0ee63a6a2986f38d112
lukasz050792 <155379384+lukasz050792@users.noreply.github.com>
2024-01-01 18:20:05 +0100

    Update length_counter.rs

--- src/apu/length_counter.rs
+++ src/apu/length_counter.rs
@@ -1,50 +1,51 @@
-use crate::save_load::*;
+use crate::common::Clock;
+use serde::{Deserialize, Serialize};
 
-pub struct LengthCounterState {
-    pub length: u8,
-    pub halt_flag: bool,
-    pub channel_enabled: bool,
+#[derive(Default, Debug, Copy, Clone, Serialize, Deserialize)]
+#[must_use]
+pub struct LengthCounter {
+    pub enabled: bool,
+    pub counter: u8, // Entry into LENGTH_TABLE
 }
 
-impl LengthCounterState{
-    pub fn new() -> LengthCounterState {
-        return LengthCounterState {
-            length: 0,
-            halt_flag: false,
-            channel_enabled: false,
+impl LengthCounter {
+    const LENGTH_TABLE: [u8; 32] = [
+        10, 254, 20, 2, 40, 4, 80, 6, 160, 8, 60, 10, 14, 12, 26, 14, 12, 16, 24, 18, 48, 20, 96,
+        22, 192, 24, 72, 26, 16, 28, 32, 30,
+    ];
+
+    pub const fn new() -> Self {
+        Self {
+            enabled: false,
+            counter: 0u8,
         }
     }
 
-    pub fn clock(&mut self) {
-        if self.channel_enabled {
-            if self.length > 0 && !(self.halt_flag) {
-                self.length -= 1;
-            }
+    #[inline]
+    #[must_use]
+    pub const fn counter(&self) -> u8 {
+        self.counter
+    }
+
+    #[inline]
+    pub fn load_value(&mut self, val: u8) {
+        self.counter = Self::LENGTH_TABLE[(val >> 3) as usize]; // D7..D3
+    }
+
+    #[inline]
+    pub fn write_ctrl(&mut self, val: u8) {
+        self.enabled = (val >> 5) & 1 == 0; // !D5
+    }
+}
+
+impl Clock for LengthCounter {
+    #[inline]
+    fn clock(&mut self) -> usize {
+        if self.enabled && self.counter > 0 {
+            self.counter -= 1;
+            1
         } else {
-            self.length = 0;
+            0
         }
-    }
-
-    pub fn set_length(&mut self, index: u8) {
-        if self.channel_enabled {
-            let table = [
-                10, 254, 20,  2, 40,  4, 80,  6, 160,  8, 60, 10, 14, 12, 26, 14,
-                12, 16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30];
-            self.length = table[index as usize];
-        } else {
-            self.length = 0
-        }
-    }
-
-    pub fn save_state(&self, buff: &mut Vec<u8>) {
-        save_u8(buff, self.length);
-        save_bool(buff, self.halt_flag);
-        save_bool(buff, self.channel_enabled);
-    }
-
-    pub fn load_state(&mut self, buff: &mut Vec<u8>) {
-        load_bool(buff, &mut self.channel_enabled);
-        load_bool(buff, &mut self.halt_flag);
-        load_u8(buff, &mut self.length);
     }
 }
